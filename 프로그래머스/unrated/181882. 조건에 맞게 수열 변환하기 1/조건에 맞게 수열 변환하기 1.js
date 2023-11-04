7a4f54306b0eb95467fc9076981cda78c4058ef3
Minhyuk Song <98685266+BeMatthewsong@users.noreply.github.com>
2023-11-04 22:09:09 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 3.83 ms, Memory: 42.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,8 @@
+const solution = arr => {
+    return arr.map(v => v >= 50 && v % 2 == 0 
+                ? v / 2 
+                : v < 50 && v % 2 == 1 
+                    ? v * 2 
+                    : v
+   );
+}
